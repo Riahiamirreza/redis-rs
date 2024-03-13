f439d91843645cb8486ebad7c56bb859e5459c7c
Riahiamirreza <riahiamirreza79@gmail.com>
2024-03-13 19:50:41 +0330

    implement PING command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::{Write, Read};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,12 +9,19 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(s) => {
+                handle(s);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+
+fn handle(mut s: TcpStream) {
+    let mut buf = [0u8; 1024];
+    let _ = s.read(&mut buf).expect("Could not read from client");
+    s.write_all(b"+PONG\r\n");
 }
