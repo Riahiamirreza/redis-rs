211901a35f8a277eb46bd4c85f4d8f12263e5710
Riahiamirreza <riahiamirreza79@gmail.com>
2025-03-06 18:22:21 +0330

    Implement rdb file decoder

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
     net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
     thread, time,
+    fs,
 };
 
 use clap::Parser;
@@ -27,12 +28,290 @@ struct State {
     storage: Mutex<HashMap<String, (Option<time::Instant>, Vec<u8>)>>,
 }
 
+struct RDBObject {
+    version: String,
+    path: String,
+    metadata: HashMap<String, String>,
+    storage: HashMap<String, (Option<time::Instant>, Vec<u8>)>,
+}
+
+enum RDBFileObject {
+    Str(Vec<u8>),
+    Integer(i64)
+}
+
+/*
+It implements this: https://rdb.fnordig.de/file_format.html#length-encoding
+*/
+fn decode_object(data:&[u8]) -> Option<(RDBFileObject, usize)> {
+    let first_byte = data[0];
+
+    match first_byte >> 6 {
+        0 => {
+            let size = (first_byte & 63) as usize;
+            Some((RDBFileObject::Str(data[1..size+1].to_vec()), size + 1))
+        }
+        1=> {
+            let second_byte = data[1];
+            let size = ((first_byte & 63) + (second_byte << 6)) as usize;
+            Some((RDBFileObject::Str(data[2..size+2].to_vec()), size + 2))
+        }
+        2 => {
+            unimplemented!()
+        }
+        3 => {
+            let remaining = first_byte & 63;
+            match remaining {
+                0 => {
+                    Some((RDBFileObject::Integer(data[1] as i64), 2 as usize))
+                }
+                1 => {
+                    let mut r = data[1] as i64;
+                    r = (r << 8) + data[2] as i64;
+                    Some((RDBFileObject::Integer(r), 3 as usize))
+                }
+                2 => {
+                    let mut r = data[1] as i64;
+                    r = (r << 8) + data[2] as i64;
+                    r = (r << 8) + data[3] as i64;
+                    r = (r << 8) + data[4] as i64;
+                    Some((RDBFileObject::Integer(r), 5 as usize))
+                }
+                3 => {
+                    unimplemented!()
+                }
+                _ => {
+                    panic!();
+                }
+            }
+        }
+        _ => None
+    }
+
+
+}
+
+fn decode_length(data: &[u8]) -> Result<(u64, usize), ()> {
+
+    let first_byte = data[0];
+
+    match first_byte >> 6 {
+        0 => {
+            let size = (first_byte & 63) as u64;
+            Ok((size, 1 as usize))
+        }
+        1=> {
+            let second_byte = data[1];
+            let size = ((first_byte & 63) + (second_byte << 6)) as u64;
+            Ok((size, 2))
+        }
+        2 => {
+            unimplemented!()
+        }
+        3 => {
+            let remaining = first_byte & 63;
+            match remaining {
+                0 => {
+                    Ok(((data[1] as u64), 2 as usize))
+                }
+                1 => {
+                    let mut r = data[1] as u64;
+                    r = (r << 8) + data[2] as u64;
+                    Ok((r, 3 as usize))
+                }
+                2 => {
+                    let mut r = data[1] as u64;
+                    r = (r << 8) + data[2] as u64;
+                    r = (r << 8) + data[3] as u64;
+                    r = (r << 8) + data[4] as u64;
+                    Ok((r, 5 as usize))
+                }
+                3 => {
+                    unimplemented!()
+                }
+                what => {
+                    println!("{}", what);
+                    panic!();
+                }
+            }
+        }
+        _ => panic!()
+    }
+}
+
+impl RDBObject {
+    fn from_file(path: &str) -> Result<Self, ()> {
+        let mut i: usize = 0;
+        let Ok(data) = fs::read(path) else {
+            return Err(());
+        };
+
+        // Checking header:
+        if data.len() < 5 || data[i..i+5] != [82, 69, 68, 73, 83]{
+            println!("Invalid file header");
+            return Err(());
+        }
+
+        i = i + 5;
+        //skep version:
+        i = i + 4;
+
+        // start metadata section:
+        if data[i] != 250 {
+            println!("Invalid metadata section");
+            return Err(());
+        } else {
+            i = i + 1;
+        }
+
+        // Reading metadata:
+        let mut metadata: HashMap<String, String> = HashMap::new();
+        loop {
+            let mut key: String;
+            let mut value: String;
+            if let Some((obj, consumed)) = decode_object(&data[i..]) {
+                if let RDBFileObject::Str(s) = obj {
+                    i = i + consumed;
+                    key = String::from_utf8(s).expect("Invalid metadata");
+
+                } else {
+                    println!("Invalid metadata 11");
+                    return Err(());
+                }
+            } else {
+                println!("Invalid string 1");
+                return Err(());
+            }
+            if let Some((obj, consumed)) = decode_object(&data[i..]) {
+                match obj {
+                    RDBFileObject::Str(s) => {
+                        i = i + consumed;
+                        value = String::from_utf8(s).expect("Invalid metadata");
+                    }
+                    RDBFileObject::Integer(integer) => {
+                        i = i + consumed;
+                        value = format!("{}", integer);
+                    }
+                }
+            } else {
+                println!("Invalid string 2");
+                return Err(());
+            }
+            metadata.insert(key, value);
+            if data[i] == 250 {
+                i += 1;
+            } else {
+                break;
+            }
+        }
+        println!("{:?}", metadata);
+
+        i += 1;
+
+        let db_index = data[i];
+        i += 1;
+        if data[i] != 0xfb {
+            return Err(());
+        }
+        i += 1;
+        let hash_table_size: u64;
+        let expire_table_size: u64;
+        if let Ok((size, consumed)) = decode_length(&data[i..]) {
+            i += consumed;
+            hash_table_size = size;
+        } else {
+            println!("Error");
+            return Err(());
+        }
+        if let Ok((size, consumed)) = decode_length(&data[i..]) {
+            i += consumed;
+            expire_table_size = size;
+        } else {
+            println!("Error");
+            return Err(());
+        }
+
+        let mut store: HashMap<String, (Option<time::Instant>, Vec<u8>)> = HashMap::new();
+        for _ in 0..hash_table_size {
+            let type_flag = data[i];
+            i += 1;
+            match type_flag {
+                0 => {
+                    let (key, value): (String, Vec<u8>);
+                    if let Ok((size, consumed)) = decode_length(&data[i..]) {
+                        i = i + consumed;
+                        key = String::from_utf8(data[i..i+(size as usize)].to_vec()).expect("Invalid key");
+                        i = i + (size as usize);
+                        println!("{}", key);
+        
+                    } else {
+                        println!("Invalid key");
+                        return Err(());
+                    }
+                    if let Ok((size, consumed)) = decode_length(&data[i..]) {
+                        i = i + consumed;
+                        value = data[i..i+(size as usize)].to_vec();
+                        i = i + (size as usize);
+                        println!("{:?}", value);
+        
+                    } else {
+                        println!("Invalid key");
+                        return Err(());
+                    }
+                    if data[i] == 0xFC {
+                        i += 1;
+                        let expiry = u64::from_be_bytes(data[i..i+8].try_into().expect(""));
+                        let expiry = time::Instant::now() - time::Duration::from_millis(expiry);
+                        i += 8;
+                        store.insert(key, (Some(expiry), value));
+                    } else if data[i] == 0xFD {
+                        let expiry = u32::from_be_bytes(data[i..i+4].try_into().expect(""));
+                        let expiry = time::Instant::now() - time::Duration::from_secs(expiry as u64);
+                        i += 4;
+                        store.insert(key, (Some(expiry), value));
+                    } else {
+                        store.insert(key, (None, value));
+                    }
+                }
+                _ => {
+                    unimplemented!();
+                }
+            }
+        }
+        Ok(Self {
+            path: path.to_string(),
+            version: "".to_string(),
+            storage: store,
+            metadata: metadata
+        })
+    }
+}
+
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
     let mut config = Config::new();
     init_config(&mut config);
 
-    let data_storage = collections::HashMap::<String, (Option<time::Instant>, Vec<u8>)>::new();
+    let mut rdb_object: Option<RDBObject> = None;
+    match (&config.dir, &config.db_filename){
+        (Some(dir), Some(db_filename)) => {
+            let mut cloned_dir = dir.clone();
+            cloned_dir.push_str(db_filename);
+            if let Ok(o) = RDBObject::from_file(&cloned_dir) {
+                rdb_object = Some(o);
+            }
+        }
+        _ => {}
+    }
+
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+
+    let data_storage = match rdb_object {
+        Option::Some(s) => s.storage,
+        Option::None => collections::HashMap::<String, (Option<time::Instant>, Vec<u8>)>::new()
+    };
 
     let state = Arc::new(State {
         config: Mutex::new(config),
@@ -80,12 +359,13 @@ fn handle(mut stream: TcpStream, state: Arc<State>) {
                 stream.write_all(out.as_slice());
             }
             Ok(Command::Get(key)) => {
-                let storage = state.storage.lock().unwrap();
+                let mut storage = state.storage.lock().unwrap();
                 match storage.get(&key) {
                     Some((expiry, v)) => {
                         if let Some(expiry) = expiry {
                             if time::Instant::now() >= *expiry {
                                 stream.write_all(b"$-1\r\n");
+                                storage.remove(&key);
                             } else {
                                 let out = serialize_to_bulk_string(v);
                                 stream.write_all(out.as_slice());
@@ -99,6 +379,21 @@ fn handle(mut stream: TcpStream, state: Arc<State>) {
                         stream.write_all(b"$-1\r\n");
                     }
                 }
+            }
+            Ok(Command::Keys(pattern)) => {
+                let storage = state.storage.lock().unwrap();
+                let mut keys: Vec<String> = Vec::new();
+
+                if &pattern == "*" {
+                    for key in storage.keys() {
+                        keys.push(key.clone());
+                    }
+
+                } else {
+                }
+                let out = serialize_to_array(&keys.iter().map(|i| i.as_bytes()).collect::<Vec<&[u8]>>());
+                stream.write_all(out.as_slice());
+
             }
             Ok(Command::ConfigGet(key)) => {
                 if !["dir", "dbfilename"].contains(&key.as_str()) {
@@ -160,6 +455,7 @@ fn serialize_to_bulk_string(s: &[u8]) -> Vec<u8> {
     [b"$", format!("{}", s.len()).as_bytes(), b"\r\n", s, b"\r\n"].concat()
 }
 
+
 struct Config {
     dir: Option<String>,
     db_filename: Option<String>,
@@ -180,6 +476,7 @@ enum Command {
     Echo(String),
     Set(String, Vec<u8>, Option<u64>),
     Get(String),
+    Keys(String),
     ConfigGet(String),
 }
 
@@ -346,6 +643,8 @@ impl Command {
                     [RedisObject::BulkString(4, s), RedisObject::BulkString(_, o)] => {
                         if s.to_uppercase() == "ECHO".to_string() {
                             Ok(Command::Echo(o.to_string()))
+                        } else if s.to_uppercase() == "KEYS".to_string() {
+                            Ok(Command::Keys(o.to_string()))
                         } else {
                             Err(())
                         }
